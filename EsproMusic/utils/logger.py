fd1a20c0bfbca541a2d2d5db7fc713b0a97fe778
ANURAGMOD87 <lohalogogi@gmail.com>
2025-06-17 13:06:55 +0530

    Update logger.py

--- EsproMusic/utils/logger.py
+++ EsproMusic/utils/logger.py
@@ -1,54 +1,30 @@
-from pyrogram import Client, enums, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, ChatPermissions, Message
-from EsproMusic import app
-from EsproMusic.misc import SUDOERS
-import asyncio
-from pyrogram import Client, filters, enums
-from pyrogram.types import Message
-from EsproMusic import app, Userbot
-from EsproMusic.utils.database import get_assistant
-from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant, ChatAdminRequired
-from pyrogram.types import Message, ChatPrivileges
-import asyncio
-from typing import Optional
-from random import randint
-from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
 from pyrogram.enums import ParseMode
+
 from EsproMusic import app
 from EsproMusic.utils.database import is_on_off
-from config import LOG_GROUP_ID as 
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.enums import MessageEntityType
-from pyrogram.types import Message, User, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-import random
+from config import LOGGER_ID
 
 
 async def play_logs(message, streamtype):
     if await is_on_off(2):
-        chat_members = await app.get_chat_members_count(message.chat.id)
-        async for admin in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-            if admin.status == enums.ChatMemberStatus.OWNER:
-                owner_ISTKHARBOT = admin.user.mention if hasattr(admin.user, 'mention') and admin.user.mention else "Is_Hide / Deleted"
-                owner_ISTKHARBOT_id = admin.user.id if hasattr(admin.user, 'id') else "Is_Hide / Deleted"
         logger_text = f"""
 <b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
-╔════❰𝐏𝐋𝐀𝐘𝐈𝐍𝐆❱═══❍⊱❁۪۪
-<b>◈ 𝐂𝐡𝐚𝐭 ➪ </b>{message.chat.title}
-<b>◈ 𝐂𝐡𝐚𝐭 𝐈𝐝 ➪ </b> <code>{message.chat.id}</code>
-<b>◈ 𝐔𝐬𝐞𝐫 ➪ </b> {message.from_user.mention}
-<b>◈ 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 ➪ </b> @{message.from_user.username}
-<b>◈ 𝐈𝐝 ➪ </b> <code>{message.from_user.id}</code>
-<b>◈ 𝐂𝐡𝐚𝐭 𝐋𝐢𝐧𝐤 ➪ </b> @{message.chat.username}
+
+<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
+<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
+<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
 <b>◈ 𝐂𝗵𝗮𝘁 𝗠𝗲𝗺𝗯𝗲𝗿𝘀 ➪ </b> <code>{chat_members}</code>
-<b>◈ 𝐂𝗵𝗮𝘁 𝗢𝘄𝗻𝗲𝗿 ➪ </b> {owner_ISTKHARBOT} 𝐈𝐝 ➪<code>{owner_ISTKHARBOT_id}</code>
-<b>◈ 𝐒𝐞𝐚𝐫𝐜𝐡𝐞𝐝 ➪ </b> <code>{message.text.split(None, 1)[1]}</code>
-<b>◈ 𝐁𝐲 ➪ </b> {streamtype}
-╚═══❰ #𝐍𝐞𝐰𝐒𝐨𝐧𝐠 ❱══❍⊱❁۪۪"""
-        if message.chat.id != LOG_GROUP_ID:
+
+<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
+<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
+<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+
+<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
+<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+        if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
-                    chat_id=LOG_GROUP_ID,
+                    chat_id=LOGGER_ID,
                     text=logger_text,
                     parse_mode=ParseMode.HTML,
                     disable_web_page_preview=True,
